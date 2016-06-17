9454c5e94ba43db119f5a3f00cbef11691f20050
savahire8 <andrey.savkin@lamoda.ru>
2016-06-18 02:24:22 +0300

    basic helpers and empty models

---
+++ app/helpers.py
@@ -0,0 +1,20 @@
+
+def get_query_id_by_text(text, **kwargs):
+    pass
+
+
+def get_rest_id_by_query_ids(query_ids):
+    pass
+    # get query_id by text 
+    # rest_id = 1     return rest_id   
+
+
+def return_rest_info_by_rest_id(rest_id):
+    pass
+
+#  settings  
+
+
+def set_user_city(user_id, city):
+# set user city 
+    pass
