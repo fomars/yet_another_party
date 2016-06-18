683d35c6d0cbbef9eb479b850a7c35f15f90db7c
savahire8 <andrey.savkin@lamoda.ru>
2016-06-18 13:27:55 +0300

    updated models and requirements

--- app/models.py
+++ app/models.py
@@ -1,15 +1,21 @@
 from app import db
 
 
-class Attributes(db.Model):
+class Attribute(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(64), index=True)
 
+    def __repr__(self):
+        return '<Attribute {}>'.format(self.text)
 
-class Attributes_mapper(db.Model):
+
+class AttributeMapper(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
-    attribute_id = db.Column(db.Integer)
+    attribute_id = db.Column(db.Integer, db.ForeignKey('attribute.id'))
     text = db.Column(db.String(64), index=True)
     attribute_value_id = db.Column(db.Integer, index=True)
 
-
+    def __repr__(self):
+        return '<AttributeMapper {}-{}>'.format(self.text, self.attribute_value_id)
