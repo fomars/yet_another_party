51eb226abcf924f32ae10161f007782bf36a338c
savahire8 <andrey.savkin@lamoda.ru>
2016-06-19 15:00:47 +0300

    Unified migration

--- migrations/versions/bbbf3e8d28a8_.py
+++ migrations/versions/bbbf3e8d28a8_.py
@@ -1,28 +1,19 @@
 """empty message
 
-Revision ID: 3c690ff8b1f6
-Revises: 380607bbf115
-Create Date: 2016-06-19 03:22:28.000703
+Revision ID: bbbf3e8d28a8
+Revises: 034cc7be627d
+Create Date: 2016-06-19 14:56:52.865131
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '3c690ff8b1f6'
-down_revision = '380607bbf115'
-
-"""empty message
-
-Revision ID: 102506b297ef
-Revises: bf7a4a70e10b
-Create Date: 2016-06-18 18:13:36.379655
-
-"""
+revision = 'bbbf3e8d28a8'
+down_revision = '034cc7be627d'
 
 
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.sql import text
-
 
 
 def upgrade():
